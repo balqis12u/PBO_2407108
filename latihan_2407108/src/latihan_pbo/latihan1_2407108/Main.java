b3e17c6481c2fc83f4cf87fa7f421e0ab302cac9
unknown <balqis12u>
2026-02-26 08:44:53 +0700

    Menambahkan Main.java

--- latihan_2407108/src/latihan_pbo/latihan1_2407108/Main.java
+++ latihan_2407108/src/latihan_pbo/latihan1_2407108/Main.java
@@ -1,7 +1,31 @@
-package latihan_pbo.latihan1_2407108;
+class Cafe {
+    String nama;
+    String alamat;
+
+    // constructor
+    Cafe(String nama, String alamat) {
+        this.nama = nama;
+        this.alamat = alamat;
+        System.out.println("Nama Cafenya adalah = " + nama);
+        System.out.println("Alamatnya di = " + alamat);
+    }
+
+    // method
+    void buka() {
+        System.out.println("Cafe Sudah Buka");
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("praktikum 1");
+
+        Cafe cafe1 = new Cafe("Halaman", "Indramayu");
+        cafe1.buka();
+
+        Cafe cafe2 = new Cafe("Teringat", "Indramayu");
+        cafe2.buka();
+
+        Cafe cafe3 = new Cafe("KopKen", "Indramayu");
+        cafe3.buka();
     }
 }
