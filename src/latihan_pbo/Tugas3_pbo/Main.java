8ac3e17fcdb87ca7403e9c43e9afd973f16463fb
unknown <balqis12u>
2026-04-14 15:11:59 +0700

    Tuga2_pbo

---
+++ src/latihan_pbo/Tugas3_pbo/Main.java
@@ -0,0 +1,20 @@
+package latihan_pbo.Tugas3_pbo;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BangunDatar bd;
+
+        bd = new BangunDatar(3, 4);
+        bd.tampilkan();
+
+        bd = new Persegi(5);
+        bd.tampilkan();
+
+        bd = new PersegiPanjang(6, 4);
+        bd.tampilkan();
+
+        bd = new Segitiga(8, 3);
+        bd.tampilkan();
+    }
+}
